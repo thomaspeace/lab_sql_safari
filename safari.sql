5968bd0c65e83aa3645a99acd9226571ce6bfe41
Thomas Peace <thomasjpeace29@gmail.com>
2024-05-23 17:17:11 +0100

    add queries for 3 extension questions and update ERD

--- safari.sql
+++ safari.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS assignments;
 DROP TABLE IF EXISTS animals;
 DROP TABLE IF EXISTS enclosures;
-DROP TABLE staff;
+DROP TABLE staff CASCADE;
 
 
 CREATE TABLE staff(
@@ -34,9 +34,11 @@ CREATE TABLE animals(
 
 INSERT INTO enclosures (name, capacity, closed_for_maintenance) VALUES ('Big Cat Field', 20, FALSE);
 INSERT INTO enclosures (name, capacity, closed_for_maintenance) VALUES ('Swinging Monkey Enclosure', 10, FALSE);
+INSERT INTO enclosures (name, capacity, closed_for_maintenance) VALUES ('Awesome Aquarium', 100, TRUE);
 
 INSERT INTO staff (name, employee_number) VALUES ('Captain Rik', 12345);
 INSERT INTO staff (name, employee_number) VALUES ('Captain Marvel', 23456);
+INSERT INTO staff (name, employee_number) VALUES ('Diver Dan', 34567);
 
 INSERT INTO animals (name, type, age, enclosure_id) VALUES ('Tony', 'Tiger', 59, 1);
 INSERT INTO animals (name, type, age, enclosure_id) VALUES ('Simba', 'Lion', 4, 1);
@@ -56,6 +58,11 @@ INSERT INTO assignments (day, employee_id, enclosure_id) VALUES ('Tuesday', 2, 1
 INSERT INTO assignments (day, employee_id, enclosure_id) VALUES ('Wednesday', 2, 2);
 INSERT INTO assignments (day, employee_id, enclosure_id) VALUES ('Thursday', 2, 1);
 INSERT INTO assignments (day, employee_id, enclosure_id) VALUES ('Friday', 2, 2);
+INSERT INTO assignments (day, employee_id, enclosure_id) VALUES ('Monday', 3, 3);
+INSERT INTO assignments (day, employee_id, enclosure_id) VALUES ('Tuesday', 3, 3);
+INSERT INTO assignments (day, employee_id, enclosure_id) VALUES ('Wednesday', 3, 3);
+INSERT INTO assignments (day, employee_id, enclosure_id) VALUES ('Thursday', 3, 3);
+INSERT INTO assignments (day, employee_id, enclosure_id) VALUES ('Friday', 3, 3);
 
 -- names of animals in given enclosures
 SELECT name FROM animals WHERE enclosure_id = 1;
@@ -69,4 +76,36 @@ ON staff.id = assignments.employee_id
 INNER JOIN enclosures
 ON enclosure_id = enclosures.id
 ORDER BY staff.name, assignments.id;
+
+-- names of staff working in enclosures which are closed for maintenance
+SELECT DISTINCT staff.name AS employee_in_enclosure_closed_for_maintenance
+FROM staff
+INNER JOIN assignments
+ON staff.id = assignments.employee_id
+INNER JOIN enclosures
+ON enclosure_id = enclosures.id
+WHERE enclosures.closed_for_maintenance = TRUE;
+
+-- name of the enclosure where the oldest animal lives. If there are two animals who are the same age choose the first one alphabetically.
+SELECT enclosures.name AS enclosures_name,
+animals.name AS animal_name,
+animals.age AS animal_age
+FROM enclosures
+INNER JOIN animals
+ON enclosures.id = animals.enclosure_id
+ORDER BY animals.age DESC, animals.name DESC 
+LIMIT 1;
+
+-- number of different animal types a given keeper has been assigned to work with. 
+-- (Diver Dan is 0)
+SELECT staff.name,
+COUNT(DISTINCT animals.type) 
+FROM animals
+INNER JOIN enclosures
+ON enclosures.id = animals.enclosure_id
+INNER JOIN assignments
+ON enclosures.id = assignments.enclosure_id
+INNER JOIN staff
+ON staff.id = assignments.employee_id
+GROUP BY staff.name;
 
